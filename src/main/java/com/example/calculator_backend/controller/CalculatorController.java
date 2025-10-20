d13d72753927245c91007ba173226fc05057f5e0
Jana Mohamed Ibrahim Ahmed Rashed <es.jana.rashad2023@alexu.edu.eg>
2025-10-20 19:28:25 +0300

    edits

--- src/main/java/com/example/calculator_backend/controller/CalculatorController.java
+++ src/main/java/com/example/calculator_backend/controller/CalculatorController.java
@@ -2,12 +2,14 @@ package com.example.calculator_backend.controller;
 
 import com.example.calculator_backend.model.CalculationRequest;
 import com.example.calculator_backend.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class CalculatorController {
+    @Autowired
     private final CalculatorService calculatorService;
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
